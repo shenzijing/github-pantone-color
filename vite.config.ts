52b5c319832b56de2ee1d62d17918615b844865c
shenzijing <1156315281@qq.com>
2024-10-20 10:58:57 +0800

    后端渲染1020-2

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -30,6 +29,8 @@ export default defineConfig({
     })
   ],
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: {
